a5c8faf83893638f5ac6aafea63c2e3bbec63bac
PriyanshK09 <priyanshkhare4905@gmail.com>
2025-02-24 10:40:36 +0530

    Refactor findPeakElement function with optimized binary search solution; update comments on time and space complexity

--- findPeakElement.cpp
+++ findPeakElement.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-// Leetcode Solution
+// Leetcode Solution (Time Limit Exceeded)
 int findPeakElement(vector<int>& nums) {
     int n = nums.size();
     int left = 0, right = n - 1;
@@ -17,9 +17,34 @@ int findPeakElement(vector<int>& nums) {
     return left;
 }
 
+// Optimized Solution
+// Approach of this solution is to find the peak element in the array using binary search.
+// If the mid element is greater than the mid + 1 element, then the peak element will be on the left side of the mid element.
+// If the mid element is less than the mid + 1 element, then the peak element will be on the right side of the mid element.
+// So, we will update the left and right pointers accordingly.
+// We will return the left pointer as the peak element.
+
+int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+// Changes: if (nums[mid] > nums[mid + 1]) { right = mid; } else { left = mid + 1; }
+// Time Complexity: O(log(n))
+
 // User IO
 int main() {
     vector<int> nums = {1, 2, 3, 1};
     cout << findPeakElement(nums) << endl;
     return 0;
 }
+
+// Space Complexity: O(1)
